c4395e9f9790835dc33f94cb1d524484776f04ef
Sriram <sriramsubramanian23@gmail.com>
2022-12-28 21:27:08 +0530

    Printing value to console

--- src/print.rs
+++ src/print.rs
@@ -1,4 +1,26 @@
 // Public function to be called from the main function
 pub fn run () {
-    println!("Hello World! This is my first Rust code!")
+    // A simple print statement to the console
+    println!("Hello World! This is my first Rust code!");
+
+    // Printing an integer using formatting
+    println!("{}", 23);
+
+    // Printing a string with formatting
+    println!("My name is {}", "Sriram");
+
+    // Positional arguments
+    println!("I live in {0},{1}. {0} is in the state of {2}.","Coimbatore", "India", "Tamilnadu");
+
+    // Named arguments
+    println!("I speak {language}, as I'm living in {state}.",language="Tamil", state="Tamilnadu");
+
+    // Placeholder for numbers
+    println!("Binary: {:b}, Hexadecimal: {:x}, Octal: {:o}, Decimal: {}",23, 23, 23, 23);
+
+    // Placeholder for debug
+    println!("{:?}", ("Sriram", 26, true));
+
+    // Basic calculations
+    println!("23 * 7 * 96 = {}", 23*7*96);
 }
